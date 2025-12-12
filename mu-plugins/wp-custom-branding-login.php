fdee94e7b64ff9fa818991603b02f00deab1b230
alvaliving <office@alvaliving.com>
2025-12-12 14:47:19 +0000

    Create wp-custom-branding-login.php

---
+++ mu-plugins/wp-custom-branding-login.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Author: Alvaliving Design Studio
+ * Plugin: Custom Branding
+ * Description: Example of branding for WordPress login page.
+ */
+
+// Add custom logo styling to the login page
+add_action('login_enqueue_scripts', function () {
+    ?>
+    <style type="text/css">
+        #login h1 a {
+            background-image: url('INSERT-YOUR-LOGO-PATH-HERE'); /* Replace with your logo path */
+            background-size: contain;
+            width: 100%;
+            height: 120px;
+        }
+    </style>
+    <?php
+});
+
+// Change login header URL to site homepage
+add_filter('login_headerurl', function () {
+    return home_url();
+});
+
+// Change login header text to site name
+add_filter('login_headertext', function () {
+    return get_bloginfo('name');
+});
